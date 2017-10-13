46d1b6579326b3ca114afb50301579e8e7ed0e17
haazime <h4zime@gmail.com>
2017-10-13 17:40:33 +0900

    use gem

--- Gemfile
+++ Gemfile
@@ -52,3 +52,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'jquery-rails'
+
+gem 'hashigami_rails', github: 'haazime/hashigami_rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,23 +13,8 @@
 //= require rails-ujs
 //= require turbolinks
 //= require jquery3
+//= require hashigami/inlineEdit
 //= require_tree .
-
-var setupInlineEdit = function() {
-  var toggleInlineEdit = function (e) {
-    e.preventDefault();
-
-    var targetId = this.dataset.inlineEditTarget;
-    var target = document.getElementById(targetId);
-    target.classList.toggle('ie-Section-open');
-  };
-
-  var toggles = document.querySelectorAll('[data-behavior="toggle-inline-edit"]');
-  for(var i = 0; i < toggles.length; i++) {
-    var toggle = toggles[i];
-    toggle.addEventListener('click', toggleInlineEdit, false);
-  }
-}
 
 document.addEventListener('turbolinks:load', function () {
   setupInlineEdit();
